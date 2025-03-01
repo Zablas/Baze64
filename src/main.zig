eacaa780248027a15ccbeaa0c3b25ee4c581abf3
Zablas <kar.balciunas@gmail.com>
2025-03-01 21:14:35 +0200

    Add encode logic

--- src/main.zig
+++ src/main.zig
@@ -15,10 +15,70 @@ const Base64 = struct {
     pub fn _char_at(self: Base64, index: usize) u8 {
         return self._table[index];
     }
+
+    pub fn encode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
+        if (input.len == 0) {
+            return "";
+        }
+
+        const n_out = try _calc_encode_length(input);
+        var out = try allocator.alloc(u8, n_out);
+        var buf = [3]u8{ 0, 0, 0 };
+        var count: u8 = 0;
+        var iout: u64 = 0;
+
+        for (input, 0..) |_, i| {
+            buf[count] = input[i];
+            count += 1;
+            if (count == 3) {
+                out[iout] = self._char_at(buf[0] >> 2);
+                out[iout + 1] = self._char_at(((buf[0] & 0x03) << 4) + (buf[1] >> 4));
+                out[iout + 2] = self._char_at(((buf[1] & 0x0f) << 2) + (buf[2] >> 6));
+                out[iout + 3] = self._char_at(buf[2] & 0x3f);
+                iout += 4;
+                count = 0;
+            }
+        }
+
+        if (count == 1) {
+            out[iout] = self._char_at(buf[0] >> 2);
+            out[iout + 1] = self._char_at((buf[0] & 0x03) << 4);
+            out[iout + 2] = '=';
+            out[iout + 3] = '=';
+        }
+
+        if (count == 2) {
+            out[iout] = self._char_at(buf[0] >> 2);
+            out[iout + 1] = self._char_at(((buf[0] & 0x03) << 4) + (buf[1] >> 4));
+            out[iout + 2] = self._char_at((buf[1] & 0x0f) << 2);
+            out[iout + 3] = '=';
+            iout += 4;
+        }
+
+        return out;
+    }
 };
 
 pub fn main() !void {
     const base64 = Base64.init();
     const stdout = std.io.getStdOut().writer();
     try stdout.print("Character at index 28: {c}\n", .{base64._char_at(28)});
+}
+
+fn _calc_encode_length(input: []const u8) !usize {
+    if (input.len < 3) {
+        return 4;
+    }
+
+    const n_output = try std.math.divCeil(usize, input.len, 3);
+    return n_output * 4;
+}
+
+fn _calc_decode_length(input: []const u8) !usize {
+    if (input.len < 4) {
+        return 3;
+    }
+
+    const n_output = try std.math.divFloor(usize, input.len, 4);
+    return n_output * 3;
 }
